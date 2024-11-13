606a4d5e86a4d2c61975fe8f93d49b598f51a057
Clemence PAVY <clemence.pavy@etu.unicaen.fr>
2024-11-13 09:58:44 +0100

    fini accueil

--- views/accueil.php
+++ views/accueil.php
@@ -6,9 +6,10 @@
         <title>Accueil</title>
     </head>
     <body>
-        <h1> Bienvenue sur le site du gestionnaire de tâches</h1>
+        <h1> Bienvenue sur le site DangeonXplorer</h1>
         <button><a href="">Connexion</a></button>
         <button><a href="">Créer un nouveau personnage</a></button>
+        <button><a href="">Mes personnages</a></button>
         <button><a href="">Accéder à l'aventure</a></button>
     </body>
 </html>

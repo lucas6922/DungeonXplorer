794d0a416bb92853487d27f1bca075ad2fef9493
Clemence PAVY <clemence.pavy@etu.unicaen.fr>
2024-11-28 17:38:06 +0100

    regler les confli

--- index.php
+++ index.php
@@ -77,11 +77,12 @@ $router->addRoute('', 'AccueilController@index'); // Pour la racine
 $router->addRoute('index', 'AccueilController@index'); // Pour la racine
 $router->addRoute('personnages', 'PersonnageController@index'); //Pour afficher tout les personnages du compte
 $router->addRoute('personnages/{id}', 'PersonnageController@show'); // Pour afficher les détail d'un personnage par ID
-$router->addRoute('creation_compte', 'CreationCompteController@index');
-$router->addRoute('traitement_creation_compte', 'CreationCompteController@verification');
-$router->addRoute('connexion', 'ConnexionController@index');
 $router->addRoute('traitement_connexion', 'ConnexionController@verification');
 $router->addRoute('chapitre/{id}', 'ChapterController@show');
+$router->addRoute('creation_compte', 'CreationCompteController@index'); //Pour cree un compte
+$router->addRoute('connexion', 'ConnexionController@index'); //se connecter à un compte déjà existant
+$router->addRoute('traitement_creation_compte', 'CreationCompteController@verification'); //vérification des informations
+$router->addRoute('creation_personnage', 'PersonnageController@nouveau');
 
 // Appel de la méthode route
 $router->route(trim($_SERVER['REQUEST_URI'], '/'));

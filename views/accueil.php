39b068b2a47feea5e901980b2ac9465d4736db7a
lucas <lucas.lebon75@gmail.com>
2024-12-20 20:20:01 +0100

    suppresion de fichiers inutile et ajout de fichiers distinct pour le header et le footer

--- views/accueil.php
+++ views/accueil.php
@@ -1,31 +1,11 @@
-<?php
-    session_start();
-?>
+<?php include 'header.php'; ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Accueil</title>
-    </head>
-    <body>
-        <h1> Bienvenue sur le site DungeonXplorer</h1>
-        <?php
-            if (!isset($_SESSION['pla_id']) || empty($_SESSION['pla_id'])) {
-                echo '<button><a href="connexion">Connexion</a></button>
-                      <button><a href="creation_compte">Créer un compte</a></button>';
-            } else {
-                echo '<button><a href="deconnexion">Déconnexion</a></button>';
-            }
-        ?>
-        <button><a href="creation_personnage">Créer un nouveau personnage</a></button>
-        <button><a href="personnages">Mes personnages</a></button>
-        <button><a href="chapitre">Accéder à l'aventure</a></button>
-        <?php
-            if (isset($_SESSION['pla_id']) && !empty($_SESSION['pla_id'])) {
-                echo '<button><a href="infos_compte">Informations du compte</a></button>';
-            }
-        ?>
-    </body>
-</html>
+<h2>Bienvenue sur DungeonXplorer !</h2>
+
+<div class="button-accueil">
+    <button><a href="creation_personnage">Créer un nouveau personnage</a></button>
+    <button><a href="personnages">Mes personnages</a></button>
+    <button><a href="chapitre">Accéder à l'aventure</a></button>
+</div>
+
+<?php include 'footer.php'; ?>
---
+++ views/footer.php
@@ -0,0 +1,6 @@
+</main>
+<footer>
+    <p>&copy; 2024 DungeonXplorer. Tous droits réservés.</p>
+</footer>
+</body>
+</html>

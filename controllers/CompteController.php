e182533dfcf9ecfa147e27e184113040c1f7a230
lucas <lucas.lebon75@gmail.com>
2024-12-21 14:01:18 +0100

    revu de la logique de la connexion, respect du mcv

--- controllers/CompteController.php
+++ controllers/CompteController.php
@@ -120,7 +120,60 @@ class CompteController
 
     public function login()
     {
-        require_once 'views/traitement_connexion.php';
+        require_once 'database/connexion_db.php';
+
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $connexion = connect_db();
+
+        $_SESSION = $_POST;
+
+        $_SESSION['password'] = '';
+
+        if (
+            isset($_POST['pseudoOuEmail']) && isset($_POST['password']) &&
+            !empty($_POST['pseudoOuEmail']) && !empty($_POST['password'])
+        ) {
+
+            $pseudoOuEmail = trim(strip_tags($_POST['pseudoOuEmail']));
+            $password = trim($_POST['password']);
+
+            if (!empty($pseudoOuEmail) && !empty($password)) {
+                try {
+                    // Vérifier que la paire (email, password) OU (pseudo, password)
+                    // est présente dans la base de données
+
+                    $select = $connexion->query("select pla_id, pla_firstname, pla_surname, pla_mail, pla_pseudo, pla_passwd from PLAYER where pla_mail = '$pseudoOuEmail' or pla_pseudo = '$pseudoOuEmail'");
+                    $enregistrement = $select->fetch(PDO::FETCH_OBJ);
+
+                    if ($enregistrement && password_verify($password, $enregistrement->pla_passwd)) {
+                        // Le compte existe
+                        $_SESSION['pla_id'] = $enregistrement->pla_id;
+                        $_SESSION['pla_firstname'] = $enregistrement->pla_firstname;
+                        $_SESSION['pla_surname'] = $enregistrement->pla_surname;
+                        $_SESSION['pla_mail'] = $enregistrement->pla_mail;
+                        $_SESSION['pla_pseudo'] = $enregistrement->pla_pseudo;
+                        header('Location: ./');
+                    } else {
+                        $_SESSION['connexion_error'] = "Ce compte n'existe pas";
+                        header('Location: connexion');
+                    }
+                } catch (Exception $e) {
+                    $_SESSION['connexion_error'] = "Une erreur est survenue lors de la connexion : " . $e->getMessage();
+                    header('Location: connexion');
+                }
+            } else {
+                $_SESSION['connexion_error'] = "Les caractères < et > ne sont pas autorisés pour l'email";
+                header('Location: connexion');
+            }
+        } else {
+            $_SESSION['connexion_error'] = "Vous n'avez pas indiqué toutes vos informations";
+            header('Location: connexion');
+        }
+
+        $connexion = null;
     }
 
     public function logout()
--- views/form_connexion.php
+++ views/form_connexion.php
@@ -1,51 +1,30 @@
 <?php
-    // form_connexion.php
-    // formulaire de connexion à un compte existant
-
-    session_start();
-
-    $pseudoOuEmail = isset($_SESSION['pseudoOuEmail']) && !empty($_SESSION['pseudoOuEmail']) ? $_SESSION['pseudoOuEmail'] : '';
+include 'includes/header.php'
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Connexion</title>
-    </head>
-    <body>
-        <h1>Connexion</h1>
+<h1>Connexion</h1>
 
-        <form action="traitement_connexion" method="POST">
-            <label for="pseudoOuEmail">Votre pseudo ou adresse email</label>
-            <br>
-            <?php
-                echo '<input type="text" name="pseudoOuEmail" size="30" placeholder="Saisissez votre pseudo / email" value="' . $pseudoOuEmail . '">';
-            ?>
-            <br>
-            <br>
-            <label for="password">Votre mot de passe</label>
-            <br>
-            <input type="password" name="password" size="30" placeholder="Saisissez votre mot de passe">
-            <br>
-            <br>
-            <button type="submit">Se connecter</button>
+<form action="traitement_connexion" method="POST">
+    <label for="pseudoOuEmail">Votre pseudo ou adresse email</label>
+    <br>
+    <input type="text" name="pseudoOuEmail" size="30" placeholder="Saisissez votre pseudo / email"
+        value="<?= htmlspecialchars($_SESSION['pseudoOuEmail'] ?? '') ?>"> <br><br>
 
-            <?php
-                // affichage des erreurs
+    <label for="password">Votre mot de passe</label>
+    <br>
+    <input type="password" name="password" size="30" placeholder="Saisissez votre mot de passe">
+    <br><br>
 
-                if (isset($_SESSION['connexion_error']) && !empty($_SESSION['connexion_error'])) {
-                    echo $_SESSION['connexion_error'];
-                }
+    <button type="submit">Se connecter</button>
 
-                $_SESSION['pseudoOuEmail'] = null;
-                $_SESSION['connexion_error'] = null;
-            ?>
-        </form>
+    <?php if (!empty($_SESSION['connexion_error'])) : ?>
+        <p style="color:red;"><?= htmlspecialchars($_SESSION['connexion_error']) ?></p>
+    <?php endif; ?>
+</form>
 
-        <br>
-        <br>
-        <button><a href="creation_compte">Créer un compte</a></button>
-    </body>
-</html>
+<br>
+<br>
+
+<?php
+include 'includes/footer.php'
+?>

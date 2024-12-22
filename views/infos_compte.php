2e115dfdc1c9532058fe8aeabb60f6776939fa11
lucas <lucas.lebon75@gmail.com>
2024-12-22 18:04:57 +0100

    style pour la suppresion d'un compte + redemande à l'utilisateur confirmation pour supp compt

--- views/infos_compte.php
+++ views/infos_compte.php
@@ -1,16 +1,16 @@
 <?php include 'includes/header.php' ?>
 <h1>Informations du compte</h1>
-<?php
-echo 'Prénom : ' . $prenom;
-echo '<br>';
-echo 'Nom : ' . $nom;
-echo '<br>';
-echo 'Pseudo : ' . $pseudo;
-echo '<br>';
-echo 'Adresse email : ' . $email;
-?>
-<br>
-<br>
-<button><a href="supprimer_compte">Supprimer le compte</a></button>
+
+<div class="info-compte">
+    <p>Prénom : <?= $prenom ?></p>
+    <p>Nom : <?= $nom ?></p>
+    <p>Pseudo : <?= $pseudo ?></p>
+    <p>Adresse email : <?= $email ?></p>
+</div>
+
+
+<form action="supprimer_compte" method="POST" id="form-info-compte" onsubmit="return confirm('Êtes-vous sûr de vouloir supprimer votre compte ?');">
+    <button type="submit" class="btn-supp">Supprimer le compte</button>
+</form>
 
 <?php include 'includes/footer.php' ?>

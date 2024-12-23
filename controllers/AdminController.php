6b8a2c7695287da2fd5c790e3b97d1790a58024d
lucas <lucas.lebon75@gmail.com>
2024-12-23 21:47:37 +0100

    tableau affichant tous les joueurs dans le pannel admin

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,7 +1,8 @@
 <?php
-
+require_once 'database/connexion_db.php';
 class AdminController
 {
+
     public function showPannelAdmin()
     {
         require_once 'views/pannel_admin/pannel_admin_accueil.php';
@@ -9,6 +10,11 @@ class AdminController
 
     public function gererJoueurs()
     {
+        $connexion = connect_db();
+
+        $select = $connexion->query("SELECT * FROM PLAYER");
+        $joueurs = $select->fetchAll(PDO::FETCH_ASSOC);
+
         require_once 'views/pannel_admin/joueurs.php';
     }
 
--- views/pannel_admin/joueurs.php
+++ views/pannel_admin/joueurs.php
@@ -1,2 +1,34 @@
 <?php include 'includes/header.php'; ?>
+<table>
+    <thead>
+        <tr>
+            <th scope="col">PLA_ID</th>
+            <th scope="col">PLA_FIRSTNAME</th>
+            <th scope="col">PLA_SURNAME</th>
+            <th scope="col">PLA_MAIL</th>
+            <th scope="col">PLA_PSEUDO</th>
+            <th scope="col">ISADMIN</th>
+            <th scope="col">SUPRESSION</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($joueurs as $joueur): ?>
+            <tr>
+                <td><?php echo $joueur['PLA_ID']; ?></td>
+                <td><?php echo $joueur['PLA_FIRSTNAME']; ?></td>
+                <td><?php echo $joueur['PLA_SURNAME']; ?></td>
+                <td><?php echo $joueur['PLA_MAIL']; ?></td>
+                <td><?php echo $joueur['PLA_PSEUDO']; ?></td>
+                <td><?php echo $joueur['ISADMIN']; ?></td>
+                <td>
+                    <form action="pannel_admin/supprimer_joueur" method="post" id="form-supp-compte-panadm">
+                        <input type="hidden" name="pla_id" value="<?php echo $joueur['PLA_ID']; ?>">
+                        <button type="submit" onclick="return confirm('Êtes-vous sûr de vouloir supprimer ce joueur ?');">Supprimer</button>
+                    </form>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
 <?php include 'includes/footer.php'; ?>

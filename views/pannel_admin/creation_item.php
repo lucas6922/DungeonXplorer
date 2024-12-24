c771fe0c648d324259cf021bd3e865cc17444c79
lucas <lucas.lebon75@gmail.com>
2024-12-24 17:56:33 +0100

    formulaire d'ajout d'u item. OK

---
+++ views/pannel_admin/creation_item.php
@@ -0,0 +1,52 @@
+<?php include 'includes/header.php'; ?>
+
+<h1>Création d'un item</h1>
+
+<form action="<?php echo $baseUrl; ?>/pannel_admin/ajoutItem" method="POST" enctype="multipart/form-data">
+
+    <div>
+        <label for="ite_name">Nom de l'item :</label>
+        <input type="text" id="ite_name" name="ite_name" placeholder="Saisissez le nom de l'item" required>
+    </div>
+
+    <div>
+        <label for="ite_description">Description de l'item :</label>
+        <textarea id="ite_description" name="ite_description" placeholder="Saisissez la description de l'item"></textarea>
+    </div>
+
+    <div>
+        <label for="ite_poids">Poids de l'item :</label>
+        <input type="number" id="ite_poids" name="ite_poids" placeholder="Saisissez le poids de l'item">
+    </div>
+
+    <div>
+        <label for="ite_value">Valeur de l'item :</label>
+        <input type="number" id="ite_value" name="ite_value" placeholder="Saisissez la valeur de l'item" required>
+    </div>
+
+    <div>
+        <label for="typ_id">Type de l'item :</label>
+        <select name="typ_id" id="typ_id" required>
+            <option value="">Sélectionnez un type</option>
+            <?php foreach ($types as $type): ?>
+                <option value="<?php echo $type['TYP_ID']; ?>"><?php echo $type['TYP_LIBELLE']; ?></option>
+            <?php endforeach; ?>
+        </select>
+    </div>
+
+    <button type="submit">Créer l'item</button>
+</form>
+
+<?php
+if (isset($_SESSION['ite_creation_error']) || !empty($_SESSION['ite_creation_error'])) :
+    // Si une erreur de création existe, afficher un message d'erreur
+?>
+    <script>
+        afficherErreur("<?= htmlspecialchars($_SESSION['ite_creation_error'], ENT_QUOTES) ?>");
+    </script>
+<?php
+    unset($_SESSION['ite_creation_error']);
+endif;
+?>
+
+<?php include 'includes/footer.php'; ?>

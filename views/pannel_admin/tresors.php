7b06b30808971b5abbd04917c5f36968477e87a7
lucas <lucas.lebon75@gmail.com>
2024-12-24 17:22:55 +0100

    affichage des items OK

--- views/pannel_admin/tresors.php
+++ views/pannel_admin/tresors.php
@@ -1,2 +1,56 @@
 <?php include 'includes/header.php'; ?>
+<?php if (!empty($items)): ?>
+    <table>
+        <thead>
+            <tr>
+                <th scope="col">ITE_ID</th>
+                <th scope="col">TYP_ID</th>
+                <th scope="col">ITE_NAME</th>
+                <th scope="col">ITE_DESCRIPTION</th>
+                <th scope="col">ITE_POIDS</th>
+                <th scope="col">ITE_VALUE</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($items as $item): ?>
+                <tr>
+                    <td><?php echo $item['ITE_ID']; ?></td>
+                    <td><?php echo $item['TYP_ID']; ?></td>
+                    <td><?php echo $item['ITE_NAME']; ?></td>
+                    <td><?php echo $item['ITE_DESCRIPTION']; ?></td>
+                    <td><?php echo $item['ITE_POIDS']; ?></td>
+                    <td><?php echo $item['ITE_VALUE']; ?></td>
+                    <td>
+                        <form action="<?php echo $baseUrl; ?>/pannel_admin/supprimer_monstre" method="post" class="form-supp-pannel-panadm">
+                            <input type="hidden" name="mon_id" value="<?php echo $monster['MON_ID']; ?>">
+                            <button type="submit" onclick="return confirm('Êtes-vous sûr de vouloir supprimer ce monstre ?');">Supprimer</button>
+                        </form>
+                    </td>
+                    <td>
+                        <form action="<?php echo $baseUrl; ?>/pannel_admin/modifier_monstre" method="post" class="form-supp-pannel-panadm">
+                            <input type="hidden" name="mon_id" value="<?php echo $monster['MON_ID']; ?>">
+                            <button type="submit">Modifier</button>
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php endif ?>
+
+<div>
+    <button><a href="<?php echo $baseUrl; ?>/pannel_admin/creation_monstre">Créer un monstre</button>
+</div>
+
+<?php
+if (isset($_SESSION['error_message']) || !empty($_SESSION['error_message'])) :
+?>
+
+    <script>
+        afficherErreur("<?= $_SESSION['error_message'] ?>");
+    </script>
+<?php
+    unset($_SESSION['error_message']);
+endif;
+?>
 <?php include 'includes/footer.php'; ?>

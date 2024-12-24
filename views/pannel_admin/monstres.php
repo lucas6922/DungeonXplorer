888755726dcdf60051fbb37aaeebf749592b249b
lucas <lucas.lebon75@gmail.com>
2024-12-24 16:22:53 +0100

    suppresion d'un monstre. Pas tester -> attend que l'ajout fonctionne

--- views/pannel_admin/monstres.php
+++ views/pannel_admin/monstres.php
@@ -1,2 +1,52 @@
 <?php include 'includes/header.php'; ?>
+<?php if (!empty($monsters)): ?>
+    <table>
+        <thead>
+            <tr>
+                <th scope="col">MON_ID</th>
+                <th scope="col">LOO_ID</th>
+                <th scope="col">MON_NAME</th>
+                <th scope="col">MON_PV</th>
+                <th scope="col">MON_MANA</th>
+                <th scope="col">MON_INITIATIVE</th>
+                <th scope="col">MON_STRENGTH</th>
+                <th scope="col">MON_ATTACK</th>
+                <th scope="col">MON_XP</th>
+                <th scope="col">SUPPRESSION</th>
+                <th scope="col">MODIFICATION</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($monsters as $monster): ?>
+                <tr>
+                    <td><?php echo $monster['MON_ID']; ?></td>
+                    <td><?php echo $monster['LOO_ID']; ?></td>
+                    <td><?php echo $monster['MON_NAME']; ?></td>
+                    <td><?php echo $monster['MON_PV']; ?></td>
+                    <td><?php echo $monster['MON_MANA']; ?></td>
+                    <td><?php echo $monster['MON_INITIATIVE']; ?></td>
+                    <td><?php echo $monster['MON_STRENGTH']; ?></td>
+                    <td><?php echo $monster['MON_ATTACK']; ?></td>
+                    <td><?php echo $monster['MON_XP']; ?></td>
+                    <td>
+                        <form action="<?php echo $baseUrl; ?>/pannel_admin/supprimer_monstre" method="post" class="form-supp-pannel-panadm">
+                            <input type="hidden" name="mon_id" value="<?php echo $monster['MON_ID']; ?>">
+                            <button type="submit" onclick="return confirm('Êtes-vous sûr de vouloir supprimer ce monstre ?');">Supprimer</button>
+                        </form>
+                    </td>
+                    <td>
+                        <form action="<?php echo $baseUrl; ?>/pannel_admin/modifier_monstre" method="post" class="form-supp-pannel-panadm">
+                            <input type="hidden" name="mon_id" value="<?php echo $monster['MON_ID']; ?>">
+                            <button type="submit">Modifier</button>
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php endif ?>
+
+<div>
+    <button><a href="<?php echo $baseUrl; ?>/pannel_admin/creation_monstre">Créer un monstre</button>
+</div>
 <?php include 'includes/footer.php'; ?>

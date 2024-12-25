a76bf288de195d6fc7cadb534aeb85353ae6a0ae
lucas <lucas.lebon75@gmail.com>
2024-12-25 11:59:19 +0100

    formulaire de modification d'un item OK

--- views/pannel_admin/modifier_item.php
+++ views/pannel_admin/modifier_item.php
@@ -1,33 +1,33 @@
 <?php include 'includes/header.php'; ?>
 
-<h1>Création d'un item</h1>
+<h1>Modifier l'item <?php echo $item['ITE_NAME']; ?></h1>
 
 <form action="<?php echo $baseUrl; ?>/pannel_admin/ajoutItem" method="POST" enctype="multipart/form-data">
 
 
     <label for="ite_name">Nom de l'item *:</label>
-    <input type="text" id="ite_name" name="ite_name" placeholder="Saisissez le nom de l'item" required>
+    <input type="text" id="ite_name" name="ite_name" value="<?php echo $item['ITE_NAME']; ?>" required>
 
 
 
     <label for="ite_description">Description de l'item :</label>
-    <textarea id="ite_description" name="ite_description" placeholder="Saisissez la description de l'item"></textarea>
+    <textarea id="ite_description" name="ite_description" value="<?php echo $item['ITE_DESCRIPTION']; ?>"></textarea>
 
 
 
     <label for="ite_poids">Poids de l'item :</label>
-    <input type="number" id="ite_poids" name="ite_poids" placeholder="Saisissez le poids de l'item">
+    <input type="number" id="ite_poids" name="ite_poids" value="<?php echo $item['ITE_POIDS']; ?>">
 
 
 
     <label for="ite_value">Valeur de l'item :</label>
-    <input type="number" id="ite_value" name="ite_value" placeholder="Saisissez la valeur de l'item">
+    <input type="number" id="ite_value" name="ite_value" value="<?php echo $item['ITE_VALUE']; ?>">
 
 
 
     <label for="typ_id">Type de l'item :</label>
     <select name="typ_id" id="typ_id">
-        <option value="">Sélectionnez un type</option>
+        <option value=" <?php echo $item['ITE_ID']; ?>"><?php echo $item['ITE_NAME']; ?></option>
         <?php foreach ($types as $type): ?>
             <option value="<?php echo $type['TYP_ID']; ?>"><?php echo $type['TYP_LIBELLE']; ?></option>
         <?php endforeach; ?>
@@ -38,13 +38,13 @@
 </form>
 
 <?php
-if (isset($_SESSION['ite_creation_error']) || !empty($_SESSION['ite_creation_error'])) :
+if (isset($_SESSION['error_message']) || !empty($_SESSION['error_message'])) :
 ?>
     <script>
-        afficherErreur("<?= $_SESSION['ite_creation_error'] ?>");
+        afficherErreur("<?= $_SESSION['error_message'] ?>");
     </script>
 <?php
-    unset($_SESSION['ite_creation_error']);
+    unset($_SESSION['error_message']);
 endif;
 ?>
 

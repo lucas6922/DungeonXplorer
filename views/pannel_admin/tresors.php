37cb7d505c1a25be301de6068b2ec1b90ea01e32
lucas <lucas.lebon75@gmail.com>
2024-12-25 17:06:02 +0100

    ajout de l'affichage des différents loot sur la page des trésors

--- views/pannel_admin/tresors.php
+++ views/pannel_admin/tresors.php
@@ -1,4 +1,9 @@
 <?php include 'includes/header.php'; ?>
+<h1>TRESORS</h1>
+
+<h2>ITEMS</h2>
+
+<!--affichage des items-->
 <?php if (!empty($items)): ?>
     <table>
         <thead>
@@ -41,6 +46,49 @@
 <div>
     <button><a href="<?php echo $baseUrl; ?>/pannel_admin/creation_item">Créer un item</button>
 </div>
+
+
+
+<!--affichage des loots-->
+
+<h2>LOOTS</h2>
+<?php if (!empty($items)): ?>
+    <table>
+        <thead>
+            <tr>
+                <th scope="col">LOO_NAME</th>
+                <th scope="col">LOO_QUANTITY</th>
+                <th scope="col">SUPPRESSION</th>
+                <th scope="col">MODIFICATION</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($loots as $loot): ?>
+                <tr>
+                    <td><?php echo $loot['LOO_NAME']; ?></td>
+                    <td><?php echo $loot['LOO_QUANTITY']; ?></td>
+                    <td>
+                        <form action="<?php echo $baseUrl; ?>/pannel_admin/supprimer_item" method="post" class="form-supp-pannel-panadm">
+                            <input type="hidden" name="ite_id" value="<?php echo $item['ITE_ID']; ?>">
+                            <button type="submit" onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet item ?');">Supprimer</button>
+                        </form>
+                    </td>
+                    <td>
+                        <form action="<?php echo $baseUrl; ?>/pannel_admin/modifier_item" method="post" class="form-supp-pannel-panadm">
+                            <input type="hidden" name="ite_id" value="<?php echo $item['ITE_ID']; ?>">
+                            <button type="submit">Modifier</button>
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php endif ?>
+
+<div>
+    <button><a href="<?php echo $baseUrl; ?>/pannel_admin/creation_loot">Créer un loot</button>
+</div>
+
 
 <?php
 if (isset($_SESSION['error_message']) || !empty($_SESSION['error_message'])) :
